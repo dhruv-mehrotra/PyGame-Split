be09f83b61e8e3d6def73eac819c6ccecd491e46
dhruv-mehrotra <dhruv_m@cs.iitr.ac.in>
2022-01-30 15:29:37 +0530

    feat: reversing animation(with error)

--- classes.py
+++ classes.py
@@ -56,6 +56,8 @@ class Dino():
 
 		self.run_list = []
 		self.duck_list = []
+		self.rev_run_list = []
+		self.rev_duck_list = []
 
 		for i in range(1, 4):
 			img = pygame.image.load(f'Assets/Dino/{i}.png')
@@ -66,6 +68,19 @@ class Dino():
 			img = pygame.image.load(f'Assets/Dino/{i}.png')
 			img = pygame.transform.scale(img, (70, 38))
 			self.duck_list.append(img)
+
+		for i in range(1, 4):
+			img = pygame.image.load(f'Assets/Dino/{i}.png')
+			img = pygame.transform.flip(img, False, True)
+			img = pygame.transform.scale(img, (52, 58))
+			self.rev_run_list.append(img)
+
+		for i in range(4, 6):
+			img = pygame.image.load(f'Assets/Dino/{i}.png')
+			img = pygame.transform.flip(img, False, True)
+			img = pygame.transform.scale(img, (70, 38))
+			self.rev_duck_list.append(img)
+			
 
 		self.dead_image = pygame.image.load(f'Assets/Dino/8.png')
 		self.dead_image = pygame.transform.scale(self.dead_image, (52,58))
@@ -83,7 +98,7 @@ class Dino():
 		self.rect = self.image.get_rect()
 		self.rect.x = self.x
 		self.rect.bottom = self.base
-
+		
 		self.alive = True
 		self.counter = 0
 
@@ -91,6 +106,44 @@ class Dino():
 		if self.alive:
 			if reverse:
 				self.rect.y = 150
+				if not self.isJumping and jump:
+					self.vel = self.jumpHeight
+					self.isJumping = True
+
+				self.vel += -self.gravity
+				if self.vel <= self.jumpHeight:
+					self.vel = -self.jumpHeight
+
+				self.rect.y += self.vel
+				if self.rect.bottom > self.base:
+					self.rect.bottom = self.base
+					self.isJumping = False
+
+				if duck:
+					self.counter += 1
+					if self.counter >= 6:
+						self.index = (self.index + 1) % len(self.duck_list)
+						self.image = self.duck_list[self.index]
+						self.rect = self.image.get_rect()
+						self.rect.x = self.x
+						self.rect.bottom = self.base
+						self.counter = 0
+
+				elif self.isJumping:
+					self.index = 0
+					self.counter = 0
+					self.image = self.rev_run_list[self.index]
+				else:
+					self.counter += 1
+					if self.counter >= 4:
+						self.index = (self.index + 1) % len(self.run_list)
+						self.image = self.run_list[self.index]
+						self.rect = self.image.get_rect()
+						self.rect.x = self.x
+						self.rect.bottom = self.base
+						self.counter = 0
+
+				self.mask = pygame.mask.from_surface(self.image)
 			else:	
 				if not self.isJumping and jump:
 					self.vel = -self.jumpHeight
